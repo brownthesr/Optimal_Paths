d50e286c9975f4bc262acd824155d1dcf06d72d8
Drake Brown <drakebenbrown@gmail.com>
2025-04-24 07:52:41 -0600

    Update chasers_nonlinear.py
    Cleaned up chasers_nonlinear

--- chasers_nonlinear.py
+++ chasers_nonlinear.py
@@ -9,47 +9,59 @@ from tqdm import tqdm
 # Global progress bar
 t_f = 30
 progress_bar = tqdm(total=t_f, desc="Solving ODE")
+
+# Define a bunch of hyperparameters
+# Number of sheep
 N = 2
+# Number of Dogs
 M = 10
 # This is introduced in to the denominators for numerical stability
 eps = 0.1
 # This is the exponent on our distance function in the denominator
 # 0 corresponds to the linear case, I have found that 3 is often stable
-expo_r = 2
-expo_p = 2
+expo_r = 3
+expo_p = 3
 # This corresponds to how far away runners see pursuers
 width = 3
 # This is how fast our runner runs away from our pursuer
 d=1
 
-# Cost matrices
+# Note that each state is ordered (x_pos, y_pos, x_vel, y_vel) and each of these are stacked
+# in a single long array.
 state_dim = 4*(M + N)
 control_dim = 2*M
+
+# Define all of our cost matrices. Note that only the A in LQR is nonlinear, so 
+# we can define all of the explicitely
+# The cost on our control
 R = np.eye(control_dim)/10
+# The cost on each individual state
 Q = np.eye(state_dim)
 for i in range(N):
+    # Only penalize sheep distance from origin
     Q[4*i:4*i+4, 4*i:4*i+4] = np.diag([1000,1000, 0, 0])
 for j in range(M):
+    # Mostly penaliez dog velocity, penalizing position is more stable.
     idx = 4*N + 4*j
     Q[idx:idx+4, idx:idx+4] = np.diag([1,1, 200, 200])
 Q /= 1000
 
-# Control matrix
+# Control matrix (how our control affects the state)
 B = np.zeros((state_dim, control_dim))
 for j in range(M):
+    # We control the acceleration of our dogs
     B[4*N + 4*j + 2, 2*j] = 1
     B[4*N + 4*j + 3, 2*j + 1] = 1
 
-# The next section deals with the linearization (SKIP)
+# The next section explicitely linearizes the system around a given state
+# These derivatives will be used in the A matrix.
 # Define symbolic variables
 x0, y0, v0x, v0y = sp.symbols('rx ry rvx rvy')
 x1, y1, v1x, v1y, e1, w = sp.symbols('c1x c1y c1vx c1vy e1 w')
 d1 = (sp.sqrt((x1 - x0)**2 + (y1 - y0)**2)/w)**(e1) + eps
-
-# Alternative
 vx_i_n = (x0 - x1) / d1
 vy_i_n = (y0 - y1) / d1
-# First with respect to the runner
+# Differentiate first with respect to the runner
 dvx_drx = sp.diff(vx_i_n, x0)
 f_dvx_drx = sp.lambdify((x0, y0, x1, y1, e1, w), dvx_drx, "numpy")
 dvx_dry = sp.diff(vx_i_n, y0)
@@ -70,7 +82,6 @@ f_dvy_dpy = sp.lambdify((x0, y0, x1, y1, e1, w), dvy_dpy, "numpy")
 
 # This is computes the linearized A matrix at each step
 # The idea is that the nonlinear equation is a sum of equations that all look like (a_x-b_x)/norm(a_x-b_x)^lambda
-# so I just made it faster
 def linearize_dynamics(x):
     """This is computes the linearized A matrix at each step
         The idea is that the nonlinear equation is a sum of equations that all look like (a_x-b_x)/norm(a_x-b_x)^lambda
@@ -84,10 +95,11 @@ def linearize_dynamics(x):
     """
     # Compute the linearization of dynamics corresponding to A matrix at time t
     A_r = np.zeros((4*N,(N+M)*4))
-    # First set up the derivatives
+    # First set up the derivatives with respect to velocity (of the sheep)
     for i in range(N):
         A_r[i*4,i*4+2] = 1
         A_r[i*4+1,i*4+3] = 1
+    # Next we iterate through calculating the derivatives each with respect to individual states
     for i in range(N):
         for j in range(M):
             A_r[i*4+2,4*i] += f_dvx_drx(x[4*i],x[4*i+1],x[4*(N+j)],x[4*(N+j)+1], expo_r, width)/d
@@ -100,7 +112,7 @@ def linearize_dynamics(x):
             A_r[i*4+3,(N+j)*4] = f_dvy_dpx(x[4*i],x[4*i+1],x[4*(N+j)],x[4*(N+j)+1], expo_r, width)/d
             A_r[i*4+3,(N+j)*4+1] = f_dvy_dpy(x[4*i],x[4*i+1],x[4*(N+j)],x[4*(N+j)+1], expo_r, width)/d
     A_p = np.zeros((4*M,4*(M+N)))
-    # these correspond to the acceleration moving the position
+    # these correspond to the acceleration moving the position (of the dogs)
     for j in range(M):
         A_p[j*4,(N+j)*4+2] = 1
         A_p[j*4+1,(N+j)*4+3] = 1
@@ -138,6 +150,7 @@ def dynamics(t, x):
     progress_bar.update(t - progress_bar.n)  # Update the progress bar
     dxdt = np.zeros_like(x)
     u = compute_control(x, Q, R)
+    
     # Runners' dynamics
     for i in range(N):
         pos = x[4*i:4*i+2]
